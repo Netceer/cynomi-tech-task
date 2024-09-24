53e8c0136403d07d8ad9bd45c5e422aeed3e2026
tc <terrence@cord.com>
2024-09-24 20:22:17 +0100

    Add missing files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
-import "./index.css";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/charts/styles.css";
